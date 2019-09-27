cdfe25167001400b96aea82f9a305acf93a3384d
bryan heinz <bryan@bryanheinz.com>
2019-09-26 23:36:30 -0500

    updated for catalina (needs tested on mojave)

--- man.lbaction/Contents/Scripts/default.sh
+++ man.lbaction/Contents/Scripts/default.sh
@@ -3,4 +3,11 @@
 # LaunchBar Action Script
 #
 
-/usr/bin/man -t "$1" | /usr/bin/open -f -a /Applications/Preview.app
+os_ver=$(printf $(/usr/bin/sw_vers -productVersion) | /usr/bin/awk '{split($0,a,"."); print a[2]}')
+if [[ $os_ver -ge 15 ]]; then
+    preview="/System/Applications/Preview.app"
+else
+    preview="/Applications/Preview.app"
+fi
+
+/usr/bin/man -t "$1" | /usr/bin/open -f -a $preview
