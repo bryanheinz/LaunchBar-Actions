fd2350b3439b03c4f93bb99eca44fde3fc2735cf
Bryan Heinz <git@bryanheinz.com>
2020-12-11 19:47:28 +0000

    fixed version check for big sur

--- man.lbaction/Contents/Scripts/default.sh
+++ man.lbaction/Contents/Scripts/default.sh
@@ -3,8 +3,10 @@
 # LaunchBar Action Script
 #
 
-os_ver=$(printf $(/usr/bin/sw_vers -productVersion) | /usr/bin/awk '{split($0,a,"."); print a[2]}')
-if [[ $os_ver -ge 15 ]]; then
+os_maj=$(printf $(/usr/bin/sw_vers -productVersion) | /usr/bin/awk '{split($0,a,"."); print a[1]}')
+os_min=$(printf $(/usr/bin/sw_vers -productVersion) | /usr/bin/awk '{split($0,a,"."); print a[2]}')
+
+if [[ $os_maj -ge 11 ]] || [[ $os_min -ge 15 ]]; then
     preview="/System/Applications/Preview.app"
 else
     preview="/Applications/Preview.app"
